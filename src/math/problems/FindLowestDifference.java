338683ee1176c78e406c00074b6e02c36c920abe
rashed08 <rashed.hasan89@gmail.com>
2018-12-23 18:03:58 -0500

    first commit core java questions done

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,18 +1,42 @@
 package math.problems;
 
-public class FindLowestDifference {
+import databases.ConnectDB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LowestNumber {
+
     public static void main(String[] args) {
         /*
-         Implement in java.
-         Read this below two array. Find the lowest difference between the two array cell.
-         The lowest difference between cells is 1
-        */
-        int [] array1 = {30,12,5,9,2,20,33,1};
-        int [] array2 = {18,25,41,47,17,36,14,19};
+         * Write java solution to find the lowest number from this array.
+         * Use one of the databases from mysql or mongodb to store and to retrieve.
+         */
+        int array[] = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
+        int smallest = Integer.MAX_VALUE;
 
+        for (int i = 0; i < array.length; i++) {
+            if (smallest > array[i]) {
+                smallest = array[i];
+            }
+        }
+        System.out.println("Smallest number in array is : " + smallest);
 
+        ConnectDB connectDB = new ConnectDB();
 
+        List<String> lowestValue = new ArrayList<String>();
+        int[] array1 = new int[1];
+        array1[0] = smallest;
+        try {
+            connectDB.InsertDataFromArryToMySql(array1, "tbl_lowestNumber", "column_lowestNumber");
+            lowestValue = connectDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+            System.out.println("Data is reading from the Table (tbl_lowestNumber) and displaying to the console");
+            for (String st : lowestValue) {
+                System.out.println(st);
+            }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -3,17 +3,42 @@ package math.problems;
 public class MakePyramid {
     public static void main(String[] args) {
 
-        /*   Implement a large Pyramid of stars in the screen with java.
+        //   Implement a large Pyramid of stars in the screen with java.
+        //
+        //                    *
+        //                   * *
+        //                  * * *
+        //                 * * * *
+        //                * * * * *
+        //               * * * * * *
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
+        //Start Here
 
-        */
+        int n = 6;
+        printTriagle(n);
+    }
 
 
+    public static void printTriagle(int n) {
+
+        int a = 2 * n - 2;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < a; j++) {
+
+                System.out.print(" ");
+            }
+
+            a = a - 1;
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
         }
-  }
+    }
+}
